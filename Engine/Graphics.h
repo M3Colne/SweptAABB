6e478a2a478510bf9a84ee07e166b98386b08cb9
Gurau Nikolas <lilitroman@yahoo.com>
2019-06-28 23:35:12 +0300

    Added the drawline function

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -56,6 +56,46 @@ public:
 		PutPixel( x,y,{ unsigned char( r ),unsigned char( g ),unsigned char( b ) } );
 	}
 	void PutPixel( int x,int y,Color c );
+	void DrawLine(Vec2 p0, Vec2 p1, Color c)
+	{
+		float m = 0.0f;
+		if (p1.x != p0.x)
+		{
+			m = (p1.y - p0.y) / (p1.x - p0.x);
+		}
+
+		if (p1.x != p0.x && std::abs(m) <= 1.0f)
+		{
+			if (p0.x > p1.x)
+			{
+				std::swap(p0, p1);
+			}
+
+			const float b = p0.y - m * p0.x;
+
+			for (int x = (int)p0.x; x < (int)p1.x; x++)
+			{
+				const float y = m * (float)x + b;
+				PutPixel(x, (int)y, c);
+			}
+		}
+		else
+		{
+			if (p0.y > p1.y)
+			{
+				std::swap(p0, p1);
+			}
+
+			const float w = (p1.x - p0.x) / (p1.y - p0.y);
+			const float p = p0.x - w * p0.y;
+
+			for (int y = (int)p0.y; y < (int)p1.y; y++)
+			{
+				const float x = w * (float)y + p;
+				PutPixel((int)x, y, c);
+			}
+		}
+	}
 	~Graphics();
 private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain>				pSwapChain;
