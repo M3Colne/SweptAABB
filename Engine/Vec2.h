a5215d8488fbdc725f0b38390e6c737e0bf3d1dd
Gurau Nikolas <lilitroman@yahoo.com>
2019-09-27 18:04:39 +0300

    Made the vec2 class into a template. Now it works with any kind of date type!!!

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -1,29 +1,156 @@
 #pragma once
 
-class Vec2
+#include <cmath>
+
+template<typename T>
+class Vec2_
 {
 public:
-	Vec2() = default;
-	Vec2(float x_in, float y_in);
-	Vec2& operator=(const Vec2& rhs);
-	Vec2 operator+(const Vec2& rhs) const;
-	Vec2& operator+=(const Vec2& rhs);
-	Vec2 operator*(float rhs) const;
-	Vec2& operator*=(float rhs);
-	Vec2 operator/(float rhs) const;
-	Vec2& operator/=(float rhs);
-	Vec2 operator-(const Vec2& rhs) const;
-	Vec2& operator-=(const Vec2& rhs);
-	float GetLength() const;
-	float GetLengthSq() const;
-	Vec2& Normalize();
-	Vec2 GetNormalized() const;
-	Vec2& NormalizeTo(float normalizer); //I made this function
-	Vec2 GetNormalizedTo(float normalizer) const; //I made this function
-	float GetAngle(const float relativeToWhat) const;//-PI - +PI
-	float GetAngle() const; //0 - 2PI
-	static float GetAngleBetween(const Vec2 a, const Vec2 b);
+	Vec2_(T x_in, T y_in)
+		:
+		x(x_in),
+		y(y_in)
+	{
+	}
+	Vec2_& operator=(const Vec2_& rhs)
+	{
+		x = rhs.x;
+		y = rhs.y;
+
+		return *this;
+	}
+	Vec2_ operator+(const Vec2_& rhs) const
+	{
+		return Vec2_(x + rhs.x, y + rhs.y);
+	}
+	Vec2_& operator+=(const Vec2_& rhs)
+	{
+		return *this = *this + rhs;
+	}
+	Vec2_ operator*(T rhs) const
+	{
+		return Vec2_(x * rhs, y * rhs);
+	}
+	Vec2_& operator*=(T rhs)
+	{
+		return *this = *this * rhs;
+	}
+	Vec2_ operator/(T rhs) const
+	{
+		return Vec2_(x / rhs, y / rhs);
+	}
+	Vec2_& operator/=(T rhs)
+	{
+		return *this = *this / rhs;
+	}
+	Vec2_ operator-(const Vec2_& rhs) const
+	{
+		return Vec2_(x - rhs.x, y - rhs.y);
+	}
+	Vec2_& operator-=(const Vec2_& rhs)
+	{
+		return *this = *this - rhs;
+	}
+	T GetLength() const
+	{
+		return T(std::sqrt(GetLengthSq()));
+	}
+	T GetLengthSq() const
+	{
+		return x * x + y * y;
+	}
+	Vec2_& Normalize()
+	{
+		return *this = GetNormalized();
+	}
+	Vec2_ GetNormalized() const
+	{
+		const T len = GetLength();
+		if (len != T(0))
+		{
+			return *this * (T(1) / len);
+		}
+		return *this;
+	}
+	Vec2_& NormalizeTo(T normalizer)
+	{
+		return *this = GetNormalizedTo(normalizer);
+	}
+	Vec2_ GetNormalizedTo(T normalizer) const
+	{
+		const T len = GetLength();
+		if (len != T(0))
+		{
+			return *this * (normalizer / len);
+		}
+		return *this;
+	}
+	float GetAngle(const float relativeToWhat) const
+	{
+		//RelativeToWhat must be between 0 and 2*PI
+		//The return value can be between -PI and +PI
+		const float PI = 3.1415926f;
+		Vec2_<float> a = this->GetNormalized();
+		Vec2_<float> r(float(cos(relativeToWhat)), float(sin(relativeToWhat)));
+
+		//Finding the angle relative to 0
+		float absAngle = 0.0f;
+		if (relativeToWhat <= PI)
+		{
+			absAngle = GetAngleBetween(a, Vec2_<float>(1.0f, 0.0f));
+			if (a.y < 0)
+			{
+				absAngle = 2 * PI - absAngle;
+			}
+		}
+		else
+		{
+			absAngle = GetAngleBetween(a, Vec2_<float>(1.0f, 0.0f));
+			if (a.y < 0)
+			{
+				absAngle = 2 * PI - absAngle;
+			}
+			else
+			{
+				absAngle += 2 * PI;
+			}
+		}
+
+		if (absAngle >= relativeToWhat && absAngle <= relativeToWhat + PI)
+		{
+			return GetAngleBetween(a, r);
+		}
+		else
+		{
+			return -GetAngleBetween(a, r);
+		}
+	}
+	float GetAngle() const
+	{
+		if (this->y < 0)
+		{
+			return 6.2831853f - acos(this->GetNormalized().x);
+		}
+
+		return acos(this->GetNormalized().x);
+	}
+	float GetAngleBetween(const Vec2_ a, const Vec2_ b)
+	{
+		//It will be between 0 and PI and never negative
+		//acos((this->x * b.x + this->y * b.y) / (this->GetLength() * b.GetLength()));
+		//Dot product
+		T dp = a.x * b.x + a.y * b.y;
+		T u = a.GetLength();
+		T v = b.GetLength();
+
+		T beta = dp / (u * v);
+
+		return acos(beta);
+	}
 public:
-	float x;
-	float y;
+	T x;
+	T y;
 };
+
+typedef Vec2_<float> Vec2;
+typedef Vec2_<int> Vei2;
