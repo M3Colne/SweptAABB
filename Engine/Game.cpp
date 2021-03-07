926816904bd3ea88330f2a668bc186abffef45ef
Nikolas Gurau <ariciultepos2003@yahoo.com>
2021-03-07 16:10:25 +0200

    Moved the code from the physics functions into the .cpp file and made a way to see if the player collides with a block

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -127,10 +127,206 @@ size_t Game::GetPlayerBlockId() const
 
 bool Game::AABB_CollisionDetection(const Vec2& leftTop0, const Vec2& bottomRight0, const Vec2& leftTop1, const Vec2& bottomRight1) const
 {
-	return leftTop0.x < bottomRight1.x&&
+	return leftTop0.x < bottomRight1.x &&
 		bottomRight0.x > leftTop1.x &&
-		leftTop0.y < bottomRight1.y&&
+		leftTop0.y < bottomRight1.y &&
 		bottomRight0.y > leftTop1.y;
+}
+
+float Game::SweptAABB(const Block& block1, const Block& block2, const Vec2& vel, Vei2& normal) const
+{
+	//Get the distances
+	Vec2 entryDist(block2.leftTop.x - block1.bottomRight.x, block2.leftTop.y - block1.bottomRight.y);
+	Vec2 exitDist(block2.bottomRight.x - block1.leftTop.x, block2.bottomRight.y - block1.leftTop.y);
+	if (vel.x < 0.0f)
+	{
+		std::swap<float>(entryDist.x, exitDist.x);
+	}
+	if (vel.y < 0.0f)
+	{
+		std::swap<float>(entryDist.y, exitDist.y);
+	}
+
+	//Get the times
+	Vec2 entryTimeV(0.0f, 0.0f);
+	Vec2 exitTimeV(0.0f, 0.0f);
+	if (vel.x == 0.0f)
+	{
+		entryTimeV.x = -std::numeric_limits<float>::infinity();
+		exitTimeV.x = std::numeric_limits<float>::infinity();
+	}
+	else
+	{
+		entryTimeV.x = entryDist.x / vel.x;
+		exitTimeV.x = exitDist.x / vel.x;
+	}
+	if (vel.y == 0.0f)
+	{
+		entryTimeV.y = -std::numeric_limits<float>::infinity();
+		exitTimeV.y = std::numeric_limits<float>::infinity();
+	}
+	else
+	{
+		entryTimeV.y = entryDist.y / vel.y;
+		exitTimeV.y = exitDist.y / vel.y;
+	}
+
+	//Getting the entry and exit time points
+	const float entryTime = std::max<float>(entryTimeV.x, entryTimeV.y);
+	const float exitTime = std::min<float>(exitTimeV.x, exitTimeV.y);
+
+	//Collision test
+	if (entryTime > exitTime || (entryTimeV.x < 0.0f && entryTimeV.y < 0.0f) || entryTimeV.x > 1.0f || entryTimeV.y > 1.0f)
+	{
+		normal.x = 0;
+		normal.y = 0;
+		return 1.0f;
+	}
+	else
+	{
+		if (entryTimeV.x >= entryTimeV.y)
+		{
+			if (entryDist.x < 0)
+			{
+				normal.x = 1;
+				normal.y = 0;
+			}
+			else
+			{
+				normal.x = -1;
+				normal.y = 0;
+			}
+		}
+		else
+		{
+			if (entryDist.y < 0)
+			{
+				normal.x = 0;
+				normal.y = 1;
+			}
+			else
+			{
+				normal.x = 0;
+				normal.y = -1;
+			}
+		}
+		return entryTime;
+	}
+}
+
+void Game::CollisionFix(const Vec2& vel, std::vector<Block*>& collidableBlocks, float& collisionTime, Vei2& normal)
+{
+	//Find the smallest time for a possible collision and remove that block that we collided with
+	size_t collidingBlockId = 0;
+	for (size_t i = 0; i < collidableBlocks.size(); i++)
+	{
+		Vei2 n(0, 0);
+		const float ct = SweptAABB(Block(playerPos, playerPos + Vec2(playerWidth, playerHeight),
+			Colors::Black), *collidableBlocks[i], vel, n);
+
+		if (ct < collisionTime)
+		{
+			collisionTime = ct;
+			normal = n;
+			collidingBlockId = i;
+		}
+	}
+	if (!collidableBlocks.empty())
+	{
+		collidableBlocks.erase(collidableBlocks.begin() + collidingBlockId);
+	}
+
+	//Integration
+	//Actually this is more like a collision fix(we move the player so that it just touches the block)
+	playerPos += vel * collisionTime;
+}
+
+void Game::Physics(const float DT)
+{
+	//Forces
+	//if (!grounded)
+	//{
+	//	playerVel.y += gravityAcc * DT;//External forces(Ex: Gravity)
+	//}
+
+	//Collision detection
+	Vei2 minNormal(0, 0);
+	float minCollisionTime = 1.0f;
+	std::vector<Block*> collidableBlocks;
+
+	//This is testing if the player is even moving at all, if not then we don't do any collisionFixing
+	if (playerVel.x || playerVel.y)
+	{
+		const Vec2 playerVelFrame = playerVel * DT; //This is the velocity that we are working with, not the playerVel!
+
+		//Broadphashing
+		Vec2 bpLeftTop(playerPos);
+		Vec2 bpBottomRight(playerPos.x + playerWidth, playerPos.y + playerHeight);
+
+		if (playerVelFrame.x > 0.0f)
+		{
+			bpBottomRight.x += playerVelFrame.x;
+		}
+		if (playerVelFrame.x < 0.0f)
+		{
+			bpLeftTop.x += playerVelFrame.x;
+		}
+		if (playerVelFrame.y > 0.0f)
+		{
+			bpBottomRight.y += playerVelFrame.y;
+		}
+		if (playerVelFrame.y < 0.0f)
+		{
+			bpLeftTop.y += playerVelFrame.y;
+		}
+
+		//Get each block in the world that is in the broadphase in this vector
+		for (size_t j = 0; j < blockLength; j++)
+		{
+			for (size_t i = 0; i < blockLength; i++)
+			{
+				const size_t id = j * blockLength + i;
+				if (!(blocks[id].color == Colors::Black))
+				{
+					if (AABB_CollisionDetection(bpLeftTop, bpBottomRight, blocks[id].leftTop, blocks[id].bottomRight))
+					{
+						collidableBlocks.push_back(&blocks[id]);
+					}
+				}
+			}
+		}
+
+		//Fix the collision(Move the player just at the edge of the block)
+		CollisionFix(playerVelFrame, collidableBlocks, minCollisionTime, minNormal);
+	}
+
+	//Collision response(sliding)
+	if (minCollisionTime < 1.0f)
+	{
+		const float notDotP = playerVel.x * minNormal.y + playerVel.y * minNormal.x;
+		playerVel.x = notDotP * minNormal.y;
+		playerVel.y = notDotP * minNormal.x;
+
+		//This part of the algorithm should be fixing a problem where the player collision is fixed for only 1 direction
+		//So if you use the simple integration here(playerPos += playerVelFrame * remainingTime) it migth slide against 
+		//another wall and will not test for collision and will move through the walls while sliding into that direction.
+		//This is where we call another CollisionFix() function so it doesn't grow through neither walls
+		//Think about it, we only have 4 cases, where the player is sliding horizontally and it might encounter a wall on the
+		//left or right side which will go through it or we might sliding vertically and it might encounter a wall on the
+		//top or bottom side which will go through it.
+
+
+
+		//At this point the player MUST BE moving horizontally, vertically or not moving at all(because we are sliding)
+		//And if we encounter a wall and fix the collision we will not slide anymore(because we only go in cardinal directions)
+		if (playerVel.x || playerVel.y)
+		{
+			CollisionFix(playerVel * DT, collidableBlocks, minCollisionTime = 1.0f - minCollisionTime, minNormal);
+			//I reseted the values of minCollisionTime and minNormal because the function expects them to be so
+			//At this point, the player should be moving against two walls at most
+			//At 3 or 4 walls the velocities cancel each other out so still 2 walls at most
+		}
+	}
 }
 
 void Game::ComposeFrame()
@@ -146,7 +342,29 @@ void Game::ComposeFrame()
 	}
 
 	//Drawing the player
-	DrawRect(playerPos, playerPos + Vec2(playerWidth, playerHeight), Colors::Magenta, false);
+	if (std::any_of(std::begin(blocks), std::end(blocks), [this](const Block& b)
+	{
+		if (!(b.color == Colors::Black))
+		{
+			return AABB_CollisionDetection(playerPos, playerPos + Vec2(playerWidth, playerHeight), b.leftTop, b.bottomRight);
+		}
+		else
+		{
+			return false;
+		}
+	}))
+	{
+		intersected = true;
+	}
+
+	if (intersected)
+	{
+		DrawRect(playerPos, playerPos + Vec2(playerWidth, playerHeight), Colors::Green, false);
+	}
+	else
+	{
+		DrawRect(playerPos, playerPos + Vec2(playerWidth, playerHeight), Colors::Magenta, false);
+	}
 
 	//Debugging
 	Vec2 bpLeftTop(playerPos);
